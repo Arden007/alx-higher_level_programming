24a9fada1584ded9e7c7f28b669a500adc4b216a
Arden007 <cloveryuno664@gmail.com>
2022-11-29 22:14:31 +0200

    print min width of 2 digit betty

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for fmw in range(0, 99):
-# 02d formats an integer ( d ) to a field of minimum width 2 ( 2 ), with zero-padding on the left (leading 0 )
     print('{:02d}, '.format(fmw), end='')
 print('99')
