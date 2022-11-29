3ee4a897d57889740e17737e27dc295c83abd4a1
Arden007 <cloveryuno664@gmail.com>
2022-11-29 23:48:11 +0200

    last

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        lastnum = number % -10
+        print(lastnum, end='')
+    else:
+        lastnum = number % 10
+        print(lastnum, end='')
+    return lastnum
+    
