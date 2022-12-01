f5da0f6225d0be0fe2e4ba25c2a33744024e8e1f
Arden007 <cloveryuno664@gmail.com>
2022-12-01 21:25:25 +0200

    task 2 create func format spec

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,16 @@
 def num_arg(argv):
     count = len(argv) - 1
     if count == 0:
-        print("{:d} argument.".format(count))
+        print("{} argument.".format(count))
         return
     else:
         if count == 1:
-            print("{:d} argument:".format(count))
+            print("{} argument:".format(count))
         else:
-            print("{:d} arguments:".format(count))
+            print("{} arguments:".format(count))
         i = 1
     for i in range(count):
-        print("{:d}: {:s}".format(i + 1, argv[i + 1]))
+        print("{}: {}".format(i + 1, argv[i + 1]))
 
 if __name__ == "__main__":
     import sys
