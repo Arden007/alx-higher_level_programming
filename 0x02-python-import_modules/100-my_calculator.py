07ceb7c40af171d3521ba861a8d33a8626159980
Arden007 <cloveryuno664@gmail.com>
2022-12-01 22:42:47 +0200

    task100 create func

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+def calculator(argv):
+    i = len(argv) - 1
+    if i != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    opperand = argv[2]
+    b = int(argv[3])
+    if opperand == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, opperand, b, add(a, b)))
+    elif opperand == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, opperand, b, sub(a, b)))
+    elif opperand == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, opperand, b, mul(a, b)))
+    elif opperand == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, opperand, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    opperand = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(opperand.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, opperand[sys.argv[2]](a, b)))
+    calculator(sys.argv)
