bd5512c3e8c37d51a2c56ee6c9c7ba79654ba8a8
Arden007 <cloveryuno664@gmail.com>
2022-12-01 21:22:52 +0200

    task 2 create func

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+def num_arg(argv):
+    count = len(argv) - 1
+    if count == 0:
+        print("{:d} argument.".format(count))
+        return
+    else:
+        if count == 1:
+            print("{:d} argument:".format(count))
+        else:
+            print("{:d} arguments:".format(count))
+        i = 1
+    for i in range(count):
+        print("{:d}: {:s}".format(i + 1, argv[i + 1]))
+
 if __name__ == "__main__":
     import sys
-
-    n = len(sys.argv) - 1
-    if n == 0:
-        print("0 argument.")
-    elif n == 1:
-        print("1 argument.")
-    else:
-        print("{:d} argument.".format(n))
-    for i in range(n):
-        print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+    num_arg(sys.argv)
