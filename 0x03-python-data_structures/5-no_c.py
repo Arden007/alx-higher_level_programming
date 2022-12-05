aa24acfdcafc32ce476feb4d602a1986efce1552
Arden007 <cloveryuno664@gmail.com>
2022-12-05 22:57:26 +0200

    task 1-12 fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     no_c_str = ""
     for i in my_string:
-        if i is not 'c' and i is not 'C':
+        if i != 'c' and i != 'C':
             no_c_str += i
     return (no_c_str)
