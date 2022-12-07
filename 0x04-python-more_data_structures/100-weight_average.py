4b9341761a30e1a1e4b7ef0dd4eb88a72d6b83eb
Arden007 <cloveryuno664@gmail.com>
2022-12-07 22:23:24 +0200

    task100 betty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == None:
+    if my_list == None:
         return 0
     key = 0
     value = 0
     for i, x in my_list:
         key += i * x
         value += x
-    return (key /value)
+    return (key/value)
