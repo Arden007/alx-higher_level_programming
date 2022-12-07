657e91af2edcb84b37db2f294306ff2a9d62561b
Arden007 <cloveryuno664@gmail.com>
2022-12-07 22:38:09 +0200

    task10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     for key, value in a_dictionary.items():
         if value is max(a_dictionary.values()):
