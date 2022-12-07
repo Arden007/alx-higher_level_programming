56028e616893401523f517b9fe214ed94caa25e6
Arden007 <cloveryuno664@gmail.com>
2022-12-07 22:28:37 +0200

    task101 straight return

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_list = list(map(lambda key: list(map(lambda value: value * value, key)), matrix))
-    return new_list
+    return(list(map(lambda key: list(map(lambda value: value * value, key)), matrix)))
