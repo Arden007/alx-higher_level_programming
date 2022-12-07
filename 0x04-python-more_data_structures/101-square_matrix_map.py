8f323bd00d8102165cea7244bbd1214d350df30e
Arden007 <cloveryuno664@gmail.com>
2022-12-07 22:35:29 +0200

    task101 betty whitespace

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda key: list(map(lambda value: value * value, key)), matrix)))
+    return list(map(lambda key: list(map(lambda value: value*value, key)), matrix))
