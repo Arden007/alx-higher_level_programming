64dcfc8d7624fa1a6e1286b9f4dff2ab528d5126
Arden007 <cloveryuno664@gmail.com>
2022-12-07 21:24:53 +0200

    task10 betty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return None
-    for key,value in a_dictionary.items():
+    for key, value in a_dictionary.items():
         if value is max(a_dictionary.values()):
             return key
