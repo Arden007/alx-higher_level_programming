30584545640b5c9f32aaf073313573d77bbfede9
Arden007 <cloveryuno664@gmail.com>
2023-02-16 21:12:15 +0200

    CODE REWRITE

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,10 +2,10 @@
 -- Records are ordered by descending rating.
 SELECT `name`, SUM(`rate`) AS `rating`
   FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS t
-       ON t.`genre_id` = g.`id
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
 
        INNER JOIN `tv_show_ratings` AS r
-       ON r.`show_id` = t.`show_id`
+       ON r.`show_id` = s.`show_id`
  GROUP BY `name`
  ORDER BY `rating` DESC;
