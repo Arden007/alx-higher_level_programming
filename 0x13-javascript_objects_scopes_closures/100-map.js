8d2366c938ca79ea28b00d8bb4e32ae9d0fb558d
Arden007 <cloveryuno664@gmail.com>
2023-03-15 22:47:11 +0200

    task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const array = require('./100-data').list;
 
-const newArray = array.map((key,val) => val * key)
+const newArray = array.map((key, val) => val * key);
 console.log(array);
 console.log(newArray);
