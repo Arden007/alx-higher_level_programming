154e17ab487f4df44d0510af1808e50ab595a2da
Arden007 <cloveryuno664@gmail.com>
2023-03-15 22:03:34 +0200

    task 6-7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num) === false) {
   for (let i = 0; i < num; i++) {
       console.log('C is fun');
-    }
+  }
 } else {
   console.log('Missing number of occurrences');
 }
