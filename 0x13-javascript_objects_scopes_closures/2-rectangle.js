2377df54379681622c66d0b17557d8b83ce2dcbd
Arden007 <cloveryuno664@gmail.com>
2023-03-15 22:27:34 +0200

    task

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -7,15 +7,10 @@
  * If w or h is equal to 0 or not a positive integer, create an empty object
  */
 class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0)
-    {
-        this.width = w;
-        this.height = h;
-    }
-    else
-    {
-        {}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
