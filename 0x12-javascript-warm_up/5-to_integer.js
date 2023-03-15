c33d4b261eaf1d130714a3b47b4a32cb6e7855ee
Arden007 <cloveryuno664@gmail.com>
2023-03-15 21:55:07 +0200

    task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const num = Math.floor(Number(process.argv[2]));
 
-if (isNaN(num) === false) 
-{
-    console.log(`My number: ${num}`);
-}else
-{
-    console.log("Not a number");
+if (isNaN(num) === false) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log('Not a number');
 }
