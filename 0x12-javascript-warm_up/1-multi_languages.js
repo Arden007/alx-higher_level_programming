36adbef9e3df6ef8729ebf8e4300ad84ec177496
Arden007 <cloveryuno664@gmail.com>
2023-03-15 19:40:48 +0200

    task 0&1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const cVar = "C is fun";
+const cVar = 'C is fun';
 const pVar = 'Python is cool';
-const jVar = "JavaScript is amazing";
+const jVar = 'JavaScript is amazing';
 
 console.log(cVar);
 console.log(pVar);
