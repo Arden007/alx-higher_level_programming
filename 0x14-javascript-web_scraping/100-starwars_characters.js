cf745ba11a7d3b289cf4a344cedf4e429d104daa
Arden007 <cloveryuno664@gmail.com>
2023-04-25 21:43:56 +0200

    0x14 complete

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 
 const request = require("request");
-
 const filmId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
-
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
     return;
   }
-
   const data = JSON.parse(body);
 //   console.log(data);
   let characters = data.characters;
